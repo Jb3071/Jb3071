b74c1283670e362a2863acc56488787b6a5e7df5
Jb3071 <81830814+Jb3071@users.noreply.github.com>
2021-04-03 20:21:58 +0200

    Add files via upload

---
+++ CSC1O4 Assignment 1.py
@@ -0,0 +1,27 @@
+Python 3.9.3 (tags/v3.9.3:e723086, Apr  2 2021, 11:35:20) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> my_name = ("Jibril Abdullahi")
+>>> print(my_name)
+Jibril Abdullahi
+>>> my_department = ("Computer Science")
+>>> print(my_department)
+Computer Science
+>>> Scare = ("Boo")
+>>> print(Scare)
+Boo
+>>> Family = ("Father_Mother + Family")
+>>> print(Family)
+Father_Mother + Family
+>>> Outcome = ("12/.5")
+>>> print(Outcome)
+12/.5
+>>> Sum = ("5+5")
+>>> print(Sum)
+5+5
+>>> Remainder = ("8&3")
+>>> print(Remainder)
+8&3
+>>> x = ("(2*4)(4+2)")
+>>> print(x)
+(2*4)(4+2)
+>>> 
